6d50c0eeb7c05ac86c87a8f6afedf67c3ccf2658
Emily Loggins <em.loggins7@gmail.com>
2024-12-03 13:12:22 -0600

    typscript init

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'; // Required for Angular apps
+import { AppComponent } from './app.component';
+import { FortuneCookieComponent } from './fortune-cookie/fortune-cookie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,       // Declare the root app component
+    FortuneCookieComponent // Declare your fortune cookie component
+  ],
+  imports: [
+    BrowserModule      // Import necessary modules (like BrowserModule)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]  // Bootstraps the app with the AppComponent
+})
+export class AppModule { }
