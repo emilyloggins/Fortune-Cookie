c1bf22ec5f02ea73494adb350a35f9a2ab818697
Emily Loggins <em.loggins7@gmail.com>
2024-12-08 13:30:52 -0600

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // Required for Angular apps
 import { AppComponent } from './app.component';
 import { FortuneCookieComponent } from './fortune-cookie/fortune-cookie.component';
+import { HelloWorldComponent } from './hello-world.component';
 
 @NgModule({
   declarations: [
     AppComponent,       // Declare the root app component
-    FortuneCookieComponent // Declare your fortune cookie component
+    FortuneCookieComponent, // Declare your fortune cookie component
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule      // Import necessary modules (like BrowserModule)
